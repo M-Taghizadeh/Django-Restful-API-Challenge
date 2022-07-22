7e8d01fdd7e7396d9252d6f64623aed4300ad23a
Mohammad Taghizadeh <taghizadeh.mhmd@gmail.com>
2022-07-22 15:34:09 +0430

    bug fixed on aws tokens

--- config/device_app/views.py
+++ config/device_app/views.py
@@ -11,8 +11,8 @@ load_dotenv()
 # Get the dynamodb using boto3
 dynamodb = boto3.resource(
     "dynamodb",
-    aws_access_key_id = os.getenv("AWS_SECRET_ACCESS_KEY"),
-    aws_secret_access_key = os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY"),
     region_name = os.getenv("AWS_REGION_NAME"),
 )
 table = dynamodb.Table("Device_DB")
