31a966760e1ec3d6f8aa9878209b100819a053ad
Mohammad Taghizadeh <taghizadeh.mhmd@gmail.com>
2022-07-23 14:32:59 +0430

    bug fixed on slash problem in post urls

--- config/device_app/urls.py
+++ config/device_app/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from .views import CreateDevice_API, GetDevice_API
 
 
 urlpatterns = [
-    path("devices/", CreateDevice_API.as_view(), name="create_device"),
+    re_path(r"^devices/?$", CreateDevice_API.as_view(), name="create_device"),
     path("devices/id<pk>/", GetDevice_API.as_view(), name="get_device"),
 ]
